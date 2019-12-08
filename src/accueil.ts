c7269aa8b83555b9631aecf3f646e41f042dbf07
Omer-Faruk Kiziltoprak <omerfaruk.kiziltoprak@edu.ece.fr>
2019-12-08 21:44:29 +0100

    Update accueil.ts

--- src/accueil.ts
+++ src/accueil.ts
@@ -6,23 +6,21 @@ const app = express()
 const port: string = process.env.PORT || '8080'
 
 app.use(express.static(path.join(__dirname, '/../public')))
+app.use(require('./routes/login'));
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded())
 app.set('views', path.join(__dirname, '/../view')); 
 app.set('view engine', 'ejs');
 
-
-
 app.get(
   '/',
   (req, res) => res.render('accueil.ejs')
 )
-
-
   app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Page introuvable !!!');
   });
+  
 app.listen(port, (err: Error) => {
   if (err) {
     throw err
