7f9ba49e80a350461cf75bc478447f7665041c2c
N-maker <56386951+N-maker@users.noreply.github.com>
2019-12-11 22:43:27 +0100

    Create basic back end to recover data

--- src/routes/registration.ts
+++ src/routes/registration.ts
@@ -1,9 +1,16 @@
 import express from 'express'
 const router = express.Router()
+import bodyparser from 'body-parser'
 
+router.use(bodyparser.json())
+router.use(bodyparser.urlencoded({ extended: true }))
 
-router.get('/registration', (req: any, res: any) => {
-    res.render('registration')
-  })
+router.get('/registration', (request, response) => {
+    response.render('registration')
+})
 
-  module.exports = router;
+router.post('/registration', function (request, response) {
+    console.log(request.body);
+    response.redirect('/registration')
+})
+module.exports = router;
